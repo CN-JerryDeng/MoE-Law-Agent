5394adca8ec4b7c8fdaf5eb80698125fed3ae9f8
JerryDeng <129633776+CN-JerryDeng@users.noreply.github.com>
2025-04-30 23:26:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,18 +25,23 @@ def model_response(model_name,prompt,request):
             print(f"生成被中止，原因：{finish_reason}")
     return completion.choices[0].message.content
 
-def Court_SIM():
-    with open('log.txt', 'w', encoding='utf-8') as file:
-        file.write("流程ID:0 \n")
-        process = 0
-        #0 庭前准备：核对身份、宣布纪律、合议庭组成、询问回避 
-        #1 法庭调查：原告陈述、被告答辩、举证质证
-        #2 法庭辩论：双方发言、争议焦点
-        #3 最后陈述：原被告最后意见）
-        #4 调解阶段：询问调解意愿
-        #5 宣判：当庭或择期
-        #6 总结
+def easy_write(file_name='whole_log.txt',edit_method='a',content=''):
+    with open(file_name,edit_method) as file:
+        file.write(content)
 
-        #庭前准备
-        if process == 0:    
-            #
+def respond_and_log(model_name,prompt,request,file_name='whole_log.txt',edit_method='a'):
+    content = model_response(model_name,prompt,request)
+    easy_write(file_name,edit_method,content)
+
+def Court_SIM():
+    easy_write('whole_log.txt',edit_method='a','当前流程ID 0')
+    process = 0
+    #0 庭前准备：核对身份、宣布纪律、合议庭组成、询问回避 
+    #1 法庭调查：原告陈述、被告答辩、举证质证
+    #2 法庭辩论：双方发言、争议焦点
+    #3 最后陈述：原被告最后意见）
+    #4 调解阶段：询问调解意愿
+    #5 宣判：当庭或择期
+    #6 总结
+    #庭前准备
+    #if process == 0:    
